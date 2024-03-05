4039057d354534ce7333fa61cf03af3b30982bc1
Gustavo Saboya <saboya.gustavo@gmail.com>
2024-03-05 14:42:32 -0300

    step 3: workflows and hooks

--- schemas/Post.schema.ts
+++ schemas/Post.schema.ts
@@ -1,20 +1,45 @@
 import { allowAll } from '@keystone-6/core/access';
-import { relationship, text } from '@keystone-6/core/fields';
+import { relationship, select, text, timestamp } from '@keystone-6/core/fields';
 import { list } from '@keystone-6/core';
 
 export const Post = list({
   access: allowAll,
   fields: {
     title: text(),
+    publishedAt: timestamp(),
+    status: select({
+      options: [
+        { label: 'Published', value: 'published' },
+        { label: 'Draft', value: 'draft' },
+      ],
+      defaultValue: 'draft',
+      ui: { displayMode: 'segmented-control' },
+    }),
     author: relationship({
       ref: 'User.posts',
       ui: {
         displayMode: 'cards',
         cardFields: ['name', 'email'],
+        inlineConnect: true,
         inlineEdit: { fields: ['name', 'email'] },
         inlineCreate: { fields: ['name', 'email', 'password'] },
         linkToItem: true,
       },
     }),
+  },
+  hooks: {
+    resolveInput: ({ operation, resolvedData }) => {
+      switch (operation) {
+        case 'create':
+        case 'update':
+          if (resolvedData.status === 'published') {
+            return {
+              ...resolvedData,
+              publishedAt: new Date(),
+            };
+          }
+          return resolvedData;
+      }
+    },
   },
 });
